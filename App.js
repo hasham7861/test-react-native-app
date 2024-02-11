cbee6bf02bffd3fa3feae0422ed26636c203cc12
hasham7861 <hashamsp36@gmail.com>
2024-02-10 19:18:41 -0500

    explore dimensions

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import {useState, useEffect} from "react";
 
 export default function App() {
+
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get('window'),
+  })
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', ({window}) => {
+      setDimensions({window})
+    })
+    
+    return () => subscription?.remove();
+  });
+  
+
+  const {window} = dimensions;
+  const windowHeight = window.height;
+  const windowWidth = window.width;
+
   return (
     <View style={styles.container}>
-      <Text>First Hello World App</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box,{
+        width: windowWidth > 500 ? "70%" : "90%",
+        height: windowHeight > 600 ? "60%" : "90%"
+      }]}>
+        <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Welcome!</Text>
+      </View>
     </View>
   );
 }
 
+// const windowWidth = Dimensions.get('window').width;
+// const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    // fontSize: windowWidth > 500 ? 50: 24
+  }
+ 
 });
